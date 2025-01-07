eb768f4dd2121f99a2c528aa6a020c5c8d5650e4
mukmonthakran <s6606021621160@email.kmutnb.ac.th>
2025-01-07 18:01:37 +0700

    L4

---
+++ Lecture04/libHello.js
@@ -0,0 +1,8 @@
+const person ={
+    name: 'Anirach',
+    weight: 85
+}
+
+function sayHello(){
+    console.log("Hello World!")
+}
--- mm.js
+++
@@ -1,9 +0,0 @@
-function printHello(name){
-    console.log("Hello" + name);
-    return name + "hello!";
-}
-
-console.log(printHello.name)
-
-let result = printHello("Monthakran !");
-console.log(result);
